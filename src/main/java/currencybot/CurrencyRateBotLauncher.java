42268610b3aa44a5e00a123c2c1eebba4a725775
ZavadaAlena <blond_girl@i.ua>
2023-03-16 22:08:50 +0200

    2_ControllerBot

---
+++ src/main/java/currencybot/CurrencyRateBotLauncher.java
@@ -0,0 +1,28 @@
+package currencybot;
+
+import currencybot.controller.CurrencyRateBotController;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class CurrencyRateBotLauncher {
+
+    public static void main(String[] args) {
+
+        DailyCurrencyRateRequestService dailyCurrencyRateRequestService = new DailyCurrencyRateRequestService();
+        dailyCurrencyRateRequestService.saveCurrencyRatesToList();
+        Thread currencyRatesSaver = new Thread(dailyCurrencyRateRequestService);
+        currencyRatesSaver.start();
+
+        AutoNotificationService autoNotificationService = new AutoNotificationService();
+        Thread currencyRateSender = new Thread(autoNotificationService);
+        currencyRateSender.start();
+
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new CurrencyRateBotController());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/currencybot/controller/CurrencyRateBotController.java
+++ src/main/java/currencybot/controller/CurrencyRateBotController.java
@@ -6,6 +6,7 @@ import com.google.gson.reflect.TypeToken;
 import currencybot.dto.settings.UserSettingDto;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
@@ -16,10 +17,11 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 
-public class CurrencyBotController extends TelegramLongPollingBot {
+public class CurrencyRateBotController extends TelegramLongPollingBot {
     public static List<UserSettingDto> userSettingDtoList = settingsFromJson();
     private static final String USERNAME = "username";
     private static final String TOKEN = "token";
+    private static final String SETTINGS_CALLDATA_MARKER = "set_";
     private final ResourceBundle resourceBundle = ResourceBundle.getBundle("telegramBot");
 
 
@@ -35,16 +37,38 @@ public class CurrencyBotController extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-
         if (update.hasMessage() && update.getMessage().hasText()) {
-            SendMessage message = new SendMessage();
-            message.setChatId(update.getMessage().getChatId().toString());
-            message.setText(update.getMessage().getText());
-
+            long chatId = update.getMessage().getChatId();
+            UserSettingDto userSettingDto = new UserSettingDto(chatId);
+            if (userSettingDtoList.stream().noneMatch(item -> item.getChatId() == chatId)) {
+                userSettingDtoList.add(userSettingDto);
+            }
+            settingsToJson();
+            SendMessage message = commandResolverService.resolveMainMenuAndNotificationCommands(update.getMessage().getText(), chatId);
             try {
                 execute(message);
             } catch (TelegramApiException e) {
                 e.printStackTrace();
+            }
+        } else if (update.hasCallbackQuery()) {
+            String callData = update.getCallbackQuery().getData();
+            long messageId = update.getCallbackQuery().getMessage().getMessageId();
+            long chatId = update.getCallbackQuery().getMessage().getChatId();
+
+            if (callData.contains(SETTINGS_CALLDATA_MARKER)) {
+                EditMessageText newMessage = commandResolverService.resolveSettingsMenuCommands(callData, chatId, messageId);
+                try {
+                    execute(newMessage);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                SendMessage newMessage = commandResolverService.resolveMainMenuAndNotificationCommands(callData, chatId);
+                try {
+                    execute(newMessage);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
